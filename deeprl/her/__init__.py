67a66699603eea885a20a3416d3e763cad5de5d1
MaxGalindo150 <maximilianogalindo7@gmail.com>
2024-11-12 21:50:23 -0600

    refactor based on SB3

---
+++ deeprl/her/__init__.py
@@ -0,0 +1,7 @@
+from deeprl.her.goal_selection_strategy import GoalSelectionStrategy
+from deeprl.her.her_replay_buffer import HerReplayBuffer
+
+__all__ = [
+    "GoalSelectionStrategy", 
+    "HerReplayBuffer"
+]
